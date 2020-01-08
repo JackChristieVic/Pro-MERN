68ed0a90bb50928f52e4cdbdea7a2166e456893a
Jack Christie <C0383027@intra.camosun.bc.ca>
2020-01-07 17:57:54 -0800

    5 - installed GraphQL tools

--- server/server.js
+++ server/server.js
@@ -1,7 +1,39 @@
+let aboutMessage = 'Issue Tracker API v1.0';
 const express = require('express');
-
 // create an Express application/object
 const app = express();
+const typeDefs = `
+    
+    type Query {
+        about: String!
+    }
+
+    type Mutation {
+        setAboutMessage(message: String!): String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage: setAboutMessage,
+    }
+}
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+
+const { ApolloServer } = require('apollo-server-express');
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.applyMiddleware({ app, path: '/graphql' });
+
 const PORT = 3000;
 
 // using the static middleware function to serve static files in the public folder
